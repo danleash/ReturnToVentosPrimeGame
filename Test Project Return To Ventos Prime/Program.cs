35404da5f4c009d91831677d47802a7c943d8229
Daniel Leash <dan.leash0596@gmail.com>
2019-08-10 23:26:52 -0400

    Created a test project

---
+++ Test Project Return To Ventos Prime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreLibrary;
+using ReturnToVentosPrimeGame;
+
+namespace Test_Project_Return_To_Ventos_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use this project to make changes and test things so that it does not mess up the actual project...
+            Game newGame = new Game();
+            newGame.CreateGame();
+        }
+    }
+}
